ae106888aa25355aa0527748a3ac25a228862da7
Lassi Iiskola <AC8425@student.jamk.fi>
2023-08-01 14:49:19 +0300

    Added controllers for get all users and create new user.

---
+++ app/backend/controllers/userControllers.js
@@ -0,0 +1,68 @@
+const APIError = require('../errors/apierror')
+const BadRequest = require('../errors/badRequest')
+const { StatusCodes } = require('http-status-codes')
+const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+
+const createUser = async (req, res) => {
+  const { username, email, password, passwordCheck } = req.body
+
+  // Check if request contains all the necessary information.
+  if (!username) {
+    throw new BadRequest('username is required')
+  }
+  if (!email) {
+    throw new BadRequest('email is required')
+  }  
+  if (!password) {
+    throw new BadRequest('password is required')
+  }
+  if (!passwordCheck) {
+    throw new BadRequest('passwordCheck is required')
+  }
+  
+  // Check if usernname is already in use.
+  const userExists = await User.findOne({username})
+  // If username is already in use => throw error
+  if (userExists) {
+    throw new APIError(`User already exists: ${username}`, StatusCodes.CONFLICT)    
+  }
+ 
+  // Check if email is already in use.
+  const emailExist = await User.findOne({ email })
+  // If email is already in use => throw error
+  if (emailExist) {
+    throw new APIError(`Email already exists ${email}`, StatusCodes.CONFLICT)
+  }
+
+  // Use bcrypt to hash password  
+  if (password === passwordCheck) {
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+    const user = new User({
+      username,
+      email,
+      passwordHash
+    })
+
+    await user.save()
+    res.status(StatusCodes.CREATED).json({user})
+  } else {
+    throw new BadRequest("Password and passwordCheck don't match")
+  }
+}
+
+const getSingleUser = () => {
+
+}
+
+const getUsers = async (req, res) => {
+  const users = await User.find({})
+  res.status(StatusCodes.OK).json(users)
+}
+
+module.exports = {
+  createUser,
+  getSingleUser,
+  getUsers
+}
